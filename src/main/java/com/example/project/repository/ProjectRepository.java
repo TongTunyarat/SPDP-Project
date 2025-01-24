ed1c77c92b5ceff09009ca91b5e8834ea526acba
Tunyarat Nitihirunpattana <113578736+TongTunyarat@users.noreply.github.com>
2025-01-24 23:57:49 +0700

    update entity

--- src/main/java/com/example/project/repository/ProjectRepository.java
+++ src/main/java/com/example/project/repository/ProjectRepository.java
@@ -3,6 +3,6 @@ package com.example.project.repository;
 import com.example.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
+public interface ProjectRepository extends JpaRepository<Project, String> {
 
 }
