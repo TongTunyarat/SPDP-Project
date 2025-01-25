37fd4f0e8873cbc4d1cfd7d6836bc28f5a9189ad
Neadnapa <neadnapa.tha@student.mahidol.edu>
2025-01-25 23:19:23 +0700

    Login Page

--- src/main/java/com/example/project/repository/AccountRepository.java
+++ src/main/java/com/example/project/repository/AccountRepository.java
@@ -3,10 +3,13 @@ package com.example.project.repository;
 import com.example.project.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 // <<<<<<< Nref
 // public interface AccountRepository extends JpaRepository<Account, Integer> {
 // }
 // =======
 public interface AccountRepository extends JpaRepository<Account, String> {
+    Optional findAccountByUsername(String username);
 }
 // >>>>>>> Tong
