98c401624874205bf5a6469a8e0b25a2a791d650
Tunyarat Nitihirunpattana <113578736+TongTunyarat@users.noreply.github.com>
2025-02-01 14:13:38 +0700

    update search function

--- src/main/java/com/example/project/repository/ProjectRepository.java
+++ src/main/java/com/example/project/repository/ProjectRepository.java
@@ -3,8 +3,20 @@ package com.example.project.repository;
 
 import com.example.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
+    @Query("SELECT p FROM Project p WHERE " +
+            "LOWER(p.projectId) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.program) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.semester) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.projectCategory) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.projectTitle) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.projectDescription) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Project> searchProjects(@Param("keyword") String keyword);
 }
--- src/main/java/com/example/project/service/ShowProjectService.java
+++ src/main/java/com/example/project/service/ShowProjectService.java
@@ -1,17 +1,22 @@
 package com.example.project.service;
 
 import com.example.project.DTO.FilterResponseDTO;
+import com.example.project.entity.Project;
 import com.example.project.repository.FilterRepository;
+import com.example.project.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class FilterService {
+public class ShowProjectService {
 
     @Autowired
     private FilterRepository filterRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
 
     public List<FilterResponseDTO> getFilteredData(String program, String role ) {
         // ตรวจสอบค่า program, role  ที่ได้รับจากผู้ใช้
@@ -23,5 +28,10 @@ public class FilterService {
                 // ถ้า role ไม่ใช่ "all", กรองตาม role, ถ้าเป็น "all" ส่งค่า null
                 role != null && !role.equalsIgnoreCase("all") ? role : null
         );
+    }
+
+    public List<Project> processWord(String word) {
+        System.out.println("Search Word [Service]: " + word);
+        return projectRepository.searchProjects(word);
     }
 }
