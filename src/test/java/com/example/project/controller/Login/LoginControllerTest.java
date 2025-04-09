bc20b02797c7761d79fc189414d35abc58137ba3
Neadnapa <neadnapa.tha@student.mahidol.edu>
2025-04-09 20:45:40 +0700

    update select all schedule

--- src/test/java/com/example/project/controller/Login/LoginControllerTest.java
+++ src/test/java/com/example/project/controller/Login/LoginControllerTest.java
@@ -2,27 +2,22 @@ package com.example.project.controller.Login;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+
+import static org.mockito.Mockito.mock;
 
 class LoginControllerTest {
     LoginController loginController = new LoginController();
 
     @Test
-    void testDefaultPage() {
-        String result = loginController.defaultPage();
-        Assertions.assertEquals("replaceMeWithExpectedResult", result);
-    }
-
-    @Test
     void testLoginPage() {
-        String result = loginController.loginPage("error", "logout", null);
-        Assertions.assertEquals("replaceMeWithExpectedResult", result);
+        Model model = mock(Model.class); // mock Model
+
+        String result = loginController.loginPage("error", "logout", model);
+        Assertions.assertEquals("login", result);
     }
 
-    @Test
-    void testCheckSession() {
-        String result = loginController.checkSession();
-        Assertions.assertEquals("replaceMeWithExpectedResult", result);
-    }
+
 }
 
 //Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
